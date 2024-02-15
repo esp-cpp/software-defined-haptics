a79e20619da5b07107a403632239f0ce0e9d0c8d
William Emfinger <waemfinger@gmail.com>
2024-02-15 10:57:59 -0600

    chore: update peripheral interface for mt6701 with latest changes after base_peripheral refactor

--- main/main.cpp
+++ main/main.cpp
@@ -51,14 +51,15 @@ extern "C" void app_main(void) {
   };
 
   // now make the mt6701 which decodes the data
-  std::shared_ptr<espp::Mt6701> mt6701 = std::make_shared<espp::Mt6701>(espp::Mt6701::Config{
-      .write = std::bind(&espp::I2c::write, &i2c, std::placeholders::_1, std::placeholders::_2,
-                         std::placeholders::_3),
-      .read = std::bind(&espp::I2c::read_at_register, &i2c, std::placeholders::_1,
-                        std::placeholders::_2, std::placeholders::_3, std::placeholders::_4),
-      .velocity_filter = filter_fn,
-      .update_period = std::chrono::duration<float>(core_update_period),
-      .log_level = espp::Logger::Verbosity::WARN});
+  std::shared_ptr<espp::Mt6701> mt6701 = std::make_shared<espp::Mt6701>(
+      espp::Mt6701::Config{.write = std::bind(&espp::I2c::write, &i2c, std::placeholders::_1,
+                                              std::placeholders::_2, std::placeholders::_3),
+                           .read_register = std::bind(&espp::I2c::read_at_register, &i2c,
+                                                      std::placeholders::_1, std::placeholders::_2,
+                                                      std::placeholders::_3, std::placeholders::_4),
+                           .velocity_filter = filter_fn,
+                           .update_period = std::chrono::duration<float>(core_update_period),
+                           .log_level = espp::Logger::Verbosity::WARN});
 
   // now make the bldc driver
   std::shared_ptr<espp::BldcDriver> driver = std::make_shared<espp::BldcDriver>(
